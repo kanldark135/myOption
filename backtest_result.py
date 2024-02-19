e6c39a3fbc2cd11ce3dc6847c5d118d150d04a6f
Kwan_office <kanldark135@gmail.com>
2024-02-19 17:37:39 +0900

    v 240219_2

--- backtest_result.py
+++ backtest_result.py
@@ -253,8 +253,8 @@ no_highvol = notrade.vkospi_above_n(0.8)
 # 변동성 사이징은 눈으로 보면서 판단
 
 put_entry1 = get_date_intersect(df_monthly, weekday_entry(df_monthly, [0]), psar_trendup)
-put_entry2 = get_date_intersect(df_monthly, bbands_turndown)
-put_entry3 = get_date_intersect(df_monthly, stoch_turndown1)
+put_entry4 = get_date_intersect(df_monthly, supertrend_turnup)
+put_entry2 = get_date_intersect(df_monthly, weekday_entry(df_monthly, [0]), supertrend_trendup)
 put_entry4 = get_date_intersect(df_monthly, stoch_turndown2)
 put_entry5 = get_date_intersect(df_monthly, supertrend_turndown)
 
@@ -559,23 +559,24 @@ psar_turndown = k200.psar.rebound(pos = 's')
 psar_trendup = k200.psar.trend(pos = 'l')
 psar_trenddown = k200.psar.trend(pos = 's')
 
-supertrend_turnup = k200.supertrend.rebound(pos = 'l')
-supertrend_turndown = k200.supertrend.rebound(pos = 's')
-supertrend_trendup = k200.supertrend.trend(pos = 'l')
-supertrend_trenddown = k200.supertrend.trend(pos = 's')
+supertrend_turnup = k200.supertrend.rebound(pos = 'l', length = 7, atr_multiplier = 3)
+supertrend_turndown = k200.supertrend.rebound(pos = 's', length = 7, atr_multiplier = 3)
+supertrend_trendup = k200.supertrend.trend(pos = 'l', length = 7, atr_multiplier = 3)
+supertrend_trenddown = k200.supertrend.trend(pos = 's', length = 7, atr_multiplier = 3)
 
 bbands_turnup1 = k200.bbands.through_bbands(pos = 'l', length = 20, std = 2)
 bbands_turndown1 = k200.bbands.through_bbands(pos = 's', length = 20, std = 2)
 bbands_turnup2 = k200.bbands.through_bbands(pos = 'l', length = 60, std = 2)
 bbands_turndown2 = k200.bbands.through_bbands(pos = 's', length = 60, std = 2)
 
-rsi_turnup = k200.rsi.rebound(pos = 'l')
-rsi_turndown = k200.rsi.rebound(pos = 's')
-
 stoch_turndown1 = k200.stoch.rebound1(pos = 's', k = 10, d = 5, smooth_d = 5)
 stoch_turndown2 = k200.stoch.rebound1(pos = 's', k = 5, d = 3, smooth_d = 3)
 stoch_turnup1= k200.stoch.rebound1(pos = 'l', k = 10, d = 5, smooth_d = 5)
 stoch_turnup2 = k200.stoch.rebound1(pos = 'l', k = 5, d = 3, smooth_d = 3)
+
+rsi_turnup = k200.rsi.rebound(pos = 'l')
+rsi_turndown = k200.rsi.rebound(pos = 's')
+
 
 #1. 진입조건
 entry_condition = [
@@ -583,14 +584,16 @@ entry_condition = [
     dict(put_entry2 = get_date_intersect(df_monthly, weekday_entry(df_monthly, [0]), supertrend_trendup)),
     dict(put_entry3 = get_date_intersect(df_monthly, psar_turnup)),
     dict(put_entry4 = get_date_intersect(df_monthly, supertrend_turnup)),
-    dict(put_entry5 = get_date_intersect(df_monthly, stoch_turnup1)),
-    dict(put_entry6 = get_date_intersect(df_monthly, stoch_turnup2)),
-    dict(put_entry7 = get_date_intersect(df_monthly, rsi_turnup))
+    dict(put_entry5 = get_date_intersect(df_monthly, bbands_turnup1)),
+    dict(put_entry6 = get_date_intersect(df_monthly, bbands_turnup2)),
+    dict(put_entry7 = get_date_intersect(df_monthly, stoch_turnup1)),
+    dict(put_entry8 = get_date_intersect(df_monthly, stoch_turnup2)),
+    dict(put_entry9 = get_date_intersect(df_monthly, rsi_turnup))
 ]
 
 #2. 전략 선정 (종목 / 행사가 / 수량 / 포지션 선택)
 
-sell_put =[s
+sell_put =[
     {'P': [('delta', -0.4, -1)]},
     # {'C': [('delta', 0.3, 1)]},
     {'P': [('delta', -0.2, -1)]},
@@ -657,33 +660,36 @@ for i in range(0, len(comb), 100):
 
 #%%
 # 콜매도
-    # 테스트할 전략
+# 테스트할 전략
     
 from itertools import product
 import time
 
 psar_turnup = k200.psar.rebound(pos = 'l')
 psar_turndown = k200.psar.rebound(pos = 's')
+
 psar_trendup = k200.psar.trend(pos = 'l')
 psar_trenddown = k200.psar.trend(pos = 's')
 
-supertrend_turnup = k200.supertrend.rebound(pos = 'l')
-supertrend_turndown = k200.supertrend.rebound(pos = 's')
-supertrend_trendup = k200.supertrend.trend(pos = 'l')
-supertrend_trenddown = k200.supertrend.trend(pos = 's')
+supertrend_turnup = k200.supertrend.rebound(pos = 'l', length = 7, atr_multiplier = 3)
+supertrend_turndown = k200.supertrend.rebound(pos = 's', length = 7, atr_multiplier =
+                                               3)
+supertrend_trendup = k200.supertrend.trend(pos = 'l', length = 7, atr_multiplier = 3)
+supertrend_trenddown = k200.supertrend.trend(pos = 's', length = 7, atr_multiplier = 3)
 
 bbands_turnup1 = k200.bbands.through_bbands(pos = 'l', length = 20, std = 2)
 bbands_turndown1 = k200.bbands.through_bbands(pos = 's', length = 20, std = 2)
 bbands_turnup2 = k200.bbands.through_bbands(pos = 'l', length = 60, std = 2)
 bbands_turndown2 = k200.bbands.through_bbands(pos = 's', length = 60, std = 2)
 
+stoch_turnup1= k200.stoch.rebound1(pos = 'l', k = 10, d = 5, smooth_d = 5)
+stoch_turndown1 = k200.stoch.rebound1(pos = 's', k = 10, d = 5, smooth_d = 5)
+stoch_turnup2 = k200.stoch.rebound1(pos = 'l', k = 5, d = 3, smooth_d = 3)
+stoch_turndown2 = k200.stoch.rebound1(pos = 's', k = 5, d = 3, smooth_d = 3)
+
 rsi_turnup = k200.rsi.rebound(pos = 'l')
 rsi_turndown = k200.rsi.rebound(pos = 's')
 
-stoch_turndown1 = k200.stoch.rebound1(pos = 's', k = 10, d = 5, smooth_d = 5)
-stoch_turndown2 = k200.stoch.rebound1(pos = 's', k = 5, d = 3, smooth_d = 3)
-stoch_turnup1= k200.stoch.rebound1(pos = 'l', k = 10, d = 5, smooth_d = 5)
-stoch_turnup2 = k200.stoch.rebound1(pos = 'l', k = 5, d = 3, smooth_d = 3)
 
 #1. 진입조건
 entry_condition = [
@@ -715,10 +721,10 @@ dte_range = [
 
 #4. 청산 조건
 exit_condition = [
-    dict(call_exit1 = []),
-    dict(call_exit2 = get_date_intersect(df_monthly, psar_turnup)),
-    dict(call_exit3 = get_date_union(df_monthly, psar_turnup, k200.stoch.rebound1(pos ='l', k =5 ,d =3 , smooth_d = 3))),
-    dict(call_exit4 = get_date_union(df_monthly, psar_turnup, k200.stoch.rebound1(pos ='l', k =10 ,d =5 , smooth_d = 5)))
+    dict(put_exit1 = []),
+    dict(put_exit2 = get_date_intersect(df_monthly, psar_turnup)),
+    dict(put_exit3 = get_date_union(df_monthly, psar_turnup, k200.stoch.rebound1(pos ='l', k =5 ,d =3 , smooth_d = 3))),
+    dict(put_exit4 = get_date_union(df_monthly, psar_turnup, k200.stoch.rebound1(pos ='l', k =10 ,d =5 , smooth_d = 5)))
 ]
 
 #5. 익절 
