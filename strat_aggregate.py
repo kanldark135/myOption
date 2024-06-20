e90f886d7c3852fe6488173cd7aafcd88681ccc7
Kwan_office <kanldark135@gmail.com>
2024-06-20 17:49:47 +0900

    v 240620

--- strat_aggregate.py
+++ strat_aggregate.py
@@ -14,12 +14,12 @@ def custom_res(df_pnl, custom_weight):
 #%%  월물
 # 전략 추가/제거시 usecol 수정 필요
 
-df_monthly = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "BO:DJ", skiprows = [0])
-n = 135
+df_monthly = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "BO:DL", skiprows = [0])
 
 #1. 전략별로 loop 하는 preprocessing
 
 n_of_strats = int(df_monthly.shape[1] / 2)
+optimal_multiplier = 0.63
 
 df_pnl = pd.DataFrame()
 
@@ -60,13 +60,13 @@ def obj_function(weight, df_pnl):
     return res
 
 # Initial weight
-weight = np.ones(n_of_strats)
+weight = np.ones(n_of_strats) * optimal_multiplier
 
 # Bounds for the weights
-bound = [(0, 100) for _ in range(n_of_strats)]
+bound = [(0, 1) for _ in range(n_of_strats)]
 
 # Constraints
-const = {'type': 'eq', 'fun': lambda w: np.sum(w) - n}  # sum of all weight should be >= n_lower
+const = {'type': 'eq', 'fun': lambda w: np.sum(w) - 1}  # sum of all weight should be >= n_lower
 
 # Minimize the objective function
 opt_result = sciop.minimize(obj_function, x0=weight, args=(df_pnl), bounds=bound, constraints=[const])
@@ -79,14 +79,16 @@ res = pd.DataFrame(data = opt_result.x, index = df_pnl.columns, columns = ['weig
 print(res)
 print(pnl ,dd, ratio)
 
-pnl_int = df_pnl.multiply(np.round(opt_result.x)).sum(axis = 1)
-dd_int = pnl_int - pnl_int.cummax()
-ratio_int = pnl_int.max() / dd_int.min()
+# pnl_int = df_pnl.multiply(np.round(opt_result.x)).sum(axis = 1)
+# dd_int = pnl_int - pnl_int.cummax()
+# ratio_int = pnl_int.max() / dd_int.min()
 
-res_int = pd.DataFrame(data = np.round(opt_result.x), index = df_pnl.columns, columns = ['weight'])
+# res_int = pd.DataFrame(data = np.round(opt_result.x), index = df_pnl.columns, columns = ['weight'])
 
-res_int.to_csv("./res.csv")
-pnl_int.to_csv("./ret.csv")
+# res_int.to_csv("./res.csv")
+# pnl_int.to_csv("./ret.csv")
+
+
 #%% 주물
 
 #1. 전략별로 loop 하는 preprocessing
@@ -164,15 +166,22 @@ res_int = pd.DataFrame(data = np.round(opt_result.x), index = df_pnl.columns, co
 res_int.to_csv("./res.csv")
 pnl_int.to_csv("./ret.csv")
 
-#%% 
+#%% loop over 
+# multiple initial values
+# with 0 ~ 1 weight bound for each strategy (no leverage)
+# constraint 1 =  the sum of the weights of all strategy = 1
 
-df_weekly = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "DN:FE", skiprows = [0])
+n_size = 100
 
-n_of_strats = int(df_weekly.shape[1] / 2)
+df_ret = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "BO:DL", skiprows = [0])
+# df_ret = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "DN:FE", skiprows = [0]) : 위클리
+
+n_of_strats = int(df_ret.shape[1] / 2)
+
 df_pnl = pd.DataFrame()
 
 for i in range(n_of_strats):
-    df_dummy = df_weekly.iloc[:, 2 * i: 2 * i + 2]
+    df_dummy = df_ret.iloc[:, 2 * i: 2 * i + 2]
     df_dummy = df_dummy.iloc[1:]
     
     strat_name = df_dummy.columns[0]
@@ -194,7 +203,7 @@ for i in range(n_of_strats):
 df_drawdown = df_pnl.apply(lambda x: x - x.cummax(), axis=0)
 
 # Objective function definition
-def obj_function_max_calmar(weight, df_pnl):
+def obj_function(weight, df_pnl):
     if len(weight) != df_pnl.shape[1]:
         raise IndexError("weight size not matching size of the pnl")
     weighted_pnl = df_pnl.multiply(weight, axis=1).sum(axis=1)
@@ -207,50 +216,40 @@ def obj_function_max_calmar(weight, df_pnl):
     
     return res
 
-def obj_function_min_dd(weight, df_pnl):
-    if len(weight) != df_pnl.shape[1]:
-        raise IndexError("weight size not matching size of the pnl")
-    weighted_pnl = df_pnl.multiply(weight, axis=1).sum(axis=1)
-    weighted_drawdown = weighted_pnl - weighted_pnl.cummax()
-    
-    max_pnl = weighted_pnl.max()
-    max_drawdown = min(weighted_drawdown.min(), -0.0001)  # prevent division by zero error
-    
-    res = -max_drawdown
-    
-    return res
-
-
 # Initial weight
 weight = np.ones(n_of_strats)
+# Bounds for the weights
+bound = [(0, 1) for _ in range(n_of_strats)]
+const = {'type' : 'eq', 'fun' : lambda w : np.sum(w) - 1} 
 
-n_lower = 0
-n_higher = 150 # 운용규모 커질수록 적절하게 조정
+w_list = np.linspace(0, 1.2, 121) # initial value 에 따른 차이
 
-bound = [(n_lower, n_higher) for i in range(n_of_strats)] # no selling
-
-w_list = np.arange(n_lower, n_higher)
-
-res2_df = pd.DataFrame(columns = ['ratio', 'ratio_int'])
+df_res = pd.DataFrame(columns = ['ratio', 'ratio_int'])
+count = 0
 
 for i in w_list:
 
-    const = {'type' : 'eq', 'fun' : lambda w : np.sum(w) - i} 
-    opt_result = sciop.minimize(obj_function, x0 = weight, args = (df_pnl), bounds = bound, constraints = [const])
+    opt_result = sciop.minimize(obj_function, x0 = weight * i, args = (df_pnl), bounds = bound, constraints = [const])
 
     try:
         pnl = df_pnl.multiply(opt_result.x).sum(axis = 1)
         dd = pnl - pnl.cummax()
         ratio = pnl.max() / dd.min()
+        
     except ZeroDivisionError:
         ratio = pnl.max() / 0.0001
+    
     try:
-        pnl_int = df_pnl.multiply(np.round(opt_result.x)).sum(axis = 1)
+        pnl_int = df_pnl.multiply(np.round(n_size * opt_result.x)).sum(axis = 1)
         dd_int = pnl_int - pnl_int.cummax()
         ratio_int = pnl_int.max() / dd_int.min()
+        
     except ZeroDivisionError:
         ratio_int = pnl_int.max() / 0.0001
 
-    res2_df.loc[i] = [ratio, ratio_int]
+    count += i
+    print(count)
+
+    df_res.loc[i] = [ratio, ratio_int]
     
     
