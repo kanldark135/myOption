a2c584efc6f2eee51a566c3c7a63952876f58c2c
Kwan_office <kanldark135@gmail.com>
2024-08-30 17:39:55 +0900

    v 240830

--- strat_aggregate.py
+++ strat_aggregate.py
@@ -14,7 +14,7 @@ def custom_res(df_pnl, custom_weight):
 #%%  월물
 # 전략 추가/제거시 usecol 수정 필요
 
-df_monthly = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "BO:DL", skiprows = [0])
+df_monthly = pd.read_excel("./전략결과(240530~).xlsx", sheet_name = 'total', usecols = "BO:DL", skiprows = [0])
 n_size = 100
 optimal_multiplier = 0.82
 
@@ -94,7 +94,7 @@ pnl_int.to_csv("./ret.csv")
 
 #1. 전략별로 loop 하는 preprocessing
 
-df_weekly = pd.read_excel("./전략결과(240530).xlsx", sheet_name = 'total', usecols = "DN:FE", skiprows = [0])
+df_weekly = pd.read_excel("./전략결과(240530~).xlsx", sheet_name = 'total', usecols = "DN:FE", skiprows = [0])
 
 n_size = 100
 optimal_multiplier = 0.91
